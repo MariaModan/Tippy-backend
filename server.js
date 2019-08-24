ee64127cb51e7cde9514ac15c8a20c270affd7e6
Maria <maria.modan95@gmail.com>
2019-08-24 21:16:41 +0300

    implement signup functionality

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const db = knex({
     client: 'pg',
     connection: {
         host : '127.0.0.1',
-        user : 'postgresql',
+        user : 'postgres',
         password : '',
         database : 'tippy'
       } 
@@ -20,21 +20,48 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/', (req,res) => {
+    console.log('ok1');
+
     res.json('is working');
 })
 
 app.post('/signin', (req,res) => {
     const { email, password } = req.body;
-
+    
     res.json(email)
 })
 
 app.post('/signup', (req,res) => {
     const { name, email, password } = req.body;
+    
+    const saltRounds = 10;
+    const hash = bcrypt.hashSync(password, saltRounds);
 
-
-    res.json(name, email)
-
+    db.transaction( trx => {
+        trx('login')
+            .insert({
+                email: email,
+                hash: hash
+            })
+            .returning('userid')
+            .then( userid => {
+                return trx('users')
+                        .insert({
+                            name: name,
+                            email: email,
+                            userid: userid[0],
+                            datejoined: new Date()
+                        })
+                        .returning('*')
+            })
+            .then( user => {
+                //the promise return an array with just one element so we need to grab user[0] to access the user who just registered
+                res.json(user[0])
+            })
+            .then(trx.commit)
+            .catch(err => res.status(400).json('unable to register user'))
+    })
+    
 })
 
 const PORT = process.env.PORT || 5500;
